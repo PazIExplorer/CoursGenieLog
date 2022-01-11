144bf9f65b7aa786550a9ee306a0e65c40600b30
unknown <firedestroye.eligriv@gmail.com>
2022-01-11 08:37:42 +0100

    mise en place de la pratique

--- src/exercice1/MainExercice1.java
+++ src/exercice1/MainExercice1.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainExercice1 {
+    
 
     // Format du fichier : firstName,lastName,birthDate,gender,cityOfResidence,cityOfBirth
     // Pas de header
     public List<Person> parse(List<String> data) {
-        // A COMPLETER
+        //recuperer le fichier et le parser comme il faut
+        
         return new ArrayList<Person>();
     }
 
@@ -23,7 +25,15 @@ public class MainExercice1 {
          * - Calculer le nombre de personnes nées dans chaque ville avec les données obtenues.
          * Les données contiennent une trentaine de villes, Bonne chance !
         */
+        //on créer une variable pour le fichier
 
+        //on créer une instance de l'exercice
+        MainExerice1 monExercice1 = new MainExercice1();
+
+        //on parse avec le fichier
+        monExercice1.parse();
+
+        
 
     }
 }
